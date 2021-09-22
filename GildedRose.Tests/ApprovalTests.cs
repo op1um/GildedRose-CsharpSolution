62da091ab30266d7975d3a1bc0345d1fb38d7181
Anthony G <anthony.gorson@gmail.com>
2021-09-22 22:41:41 +0200

    Cleaning of the solution, removing of the two test classes to create a proper test project. Creation of the first unit test, 38% Test coverage.

---
+++ GildedRose.Tests/ApprovalTests.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using csharp;
+using NUnit.Framework;
+
+namespace GildedRoseTests
+{
+    [TestFixture]
+    public class ApprovalTests
+    {
+        //First test to check if the unit testing framework is running fine
+        [Test]
+        public void BasicTest()
+        {
+            Assert.True(true);
+        }
+
+        /* Since we can't know what the app does, even if the business says the app
+        already does everything they want. We need unit tests to check if this is true and to make sure
+        we don't make any regression during the refactoring */
+        [Test]
+        public void RegularItem_SellInAndQualityLower()
+        {
+            IList<Item> items = new List<Item> { new Item
+            {
+                Name = "test", SellIn = 15, Quality = 25 }
+
+            };
+            GildedRose gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+            Assert.AreEqual(14, items[0].SellIn);
+            Assert.AreEqual(24, items[0].Quality);
+        }
+    }
+}
